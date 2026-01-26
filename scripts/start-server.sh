ac4690a6ff3d881ec6c7d6d48b6d8afbd2152f21
jw <wjd10577@naver.com>
2026-01-26 17:14:41 +0900

    apply code-deploy

---
+++ scripts/start-server.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+echo "---------- 서버 배포 시작 ----------"
+cd msa-infra
+DB_HOST=$(aws ssm get-parameter \
+  --name "/prod/app/DB_HOST" \
+  --query Parameter.Value \
+  --output text)
+MYSQL_ROOT_PASSWORD=$(aws ssm get-parameter \
+  --name "/prod/app/MYSQL_ROOT_PASSWORD" \
+  --with-decryption \
+  --query Parameter.Value \
+  --output text)
+JWT_SECRET=$(aws ssm get-parameter \
+  --name "/prod/app/JWT_SECRET" \
+  --with-decryption \
+  --query Parameter.Value \
+  --output text)
+
+cat <<EOF > .env
+DB_HOST=${DB_HOST}
+MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD}
+JWT_SECRET=${JWT_SECRET}
+SPRING_PROFILES_ACTIVE=prod
+EOF
+
+docker compose pull
+docker compose up -d
+echo "---------- 서버 배포 끝 ----------"
